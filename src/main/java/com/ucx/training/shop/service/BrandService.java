39d673d8fe0b364b6b5f00dc9a9698ea52e461b2
Fati Marmullaku <fati.marmullaku@gmail.com>
2019-07-29 13:33:46 +0200

    Added repository and service for Category and Brand

---
+++ src/main/java/com/ucx/training/shop/service/BrandService.java
@@ -0,0 +1,8 @@
+package com.ucx.training.shop.service;
+
+import com.ucx.training.shop.entity.Brand;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BrandService extends BaseService<Brand, Integer> {
+}
---
+++ src/main/java/com/ucx/training/shop/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.ucx.training.shop.service;
+
+import com.ucx.training.shop.entity.Category;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService extends BaseService<Category, Integer> {
+}
