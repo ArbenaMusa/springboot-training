898cf6b4ec81afe322637fef8c15f5a18c41341b
Fati Marmullaku <fati.marmullaku@gmail.com>
2019-07-29 14:48:20 +0200

    Added Service and Repository for Phone

---
+++ src/main/java/com/ucx/training/shop/service/PhoneService.java
@@ -0,0 +1,8 @@
+package com.ucx.training.shop.service;
+
+import com.ucx.training.shop.entity.Phone;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhoneService extends BaseService<Phone, Integer> {
+}
