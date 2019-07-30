1e802b1f106338deb4f5edd906b223f8646b8a14
Jehona <jehonaakonushefci@gmail.com>
2019-07-30 15:08:50 +0200

    CategoryController, BrandController added and some minor changes

---
+++ src/main/java/com/ucx/training/shop/controller/BrandController.java
@@ -0,0 +1,87 @@
+package com.ucx.training.shop.controller;
+
+import com.ucx.training.shop.entity.Brand;
+import com.ucx.training.shop.exception.NotFoundException;
+import com.ucx.training.shop.exception.ResponseException;
+import com.ucx.training.shop.service.BrandService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Log4j2
+@RestController
+@RequestMapping("v1/brand")
+public class BrandController {
+
+    private BrandService brandService;
+
+    public BrandController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @PostMapping
+    public Brand create(@RequestBody Brand brand) throws ResponseException {
+        try {
+            Brand savedBrand = brandService.save(brand);
+            return savedBrand;
+        } catch (IllegalArgumentException e) {
+            throw new ResponseException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            throw new ResponseException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("{id}")
+    public Map<String, Integer> update(@RequestBody Brand brand, @PathVariable Integer id) throws ResponseException {
+        Map<String, Integer> responseMap = new HashMap<>();
+        try {
+            brandService.update(brand, id);
+            responseMap.put("id", id);
+        } catch (NotFoundException | IllegalArgumentException e) {
+            throw new ResponseException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            throw new ResponseException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return responseMap;
+    }
+
+    @GetMapping("{brandId}")
+    public Brand getById(@PathVariable Integer brandId) {
+        return brandService.findById(brandId);
+    }
+
+    @DeleteMapping("{id}")
+    public void remove(@PathVariable Integer id) throws ResponseException {
+        try {
+            brandService.remove(id);
+        } catch (NotFoundException | IllegalArgumentException e) {
+            throw new ResponseException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            throw new ResponseException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping
+    public List<Brand> findAllSorted(@RequestParam(required = false, defaultValue = "ASC") String direction, @RequestParam(defaultValue = "id") String... properties) throws ResponseException {
+        try {
+            return brandService.findAllSorted(direction, properties);
+        } catch (Exception e) {
+            throw new ResponseException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/paged")
+    public List<Brand> findAllPaged(@RequestParam int pageNumber, @RequestParam int pageSize) throws ResponseException {
+        try {
+            Page<Brand> brandPage = brandService.findAllPaged(pageNumber, pageSize);
+            return brandPage.getContent();
+        } catch (Exception e) {
+            throw new ResponseException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
