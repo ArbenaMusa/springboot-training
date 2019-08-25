a9212fd287efb14f9087ceb7d13fe1c481f7f5dc
Fati Marmullaku <fati.marmullaku@gmail.com>
2019-08-25 23:38:27 +0200

    Added total to order

--- src/main/java/com/ucx/training/shop/dto/PurchaseDTO.java
+++ src/main/java/com/ucx/training/shop/dto/PurchaseDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -11,6 +12,6 @@ import java.util.List;
 @AllArgsConstructor
 public class PurchaseDTO extends DTOEntity{
     private Integer customerId;
-    private Integer orderId;
     private List<CartDTO> cart;
+    private BigDecimal total;
 }
