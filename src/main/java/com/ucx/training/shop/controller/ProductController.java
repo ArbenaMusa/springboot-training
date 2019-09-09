12bd7a1515dcf58b246f13dd1bd3a8e1aeca9a58
Arbena Musa <arbenamusa@gmail.com>
2019-09-09 16:22:09 +0200

    Fix

--- src/main/java/com/ucx/training/shop/controller/ProductController.java
+++ src/main/java/com/ucx/training/shop/controller/ProductController.java
@@ -15,6 +15,7 @@ import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
